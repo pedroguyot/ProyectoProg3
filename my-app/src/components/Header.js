80fbbd0f19ce2ade5c55d4911c92d76644285d8c
Geronimo Tiscornia <geronimotiscornia@MacBook-Air-de-Geronimo.local>
2025-04-09 19:07:20 -0300

    termine el header

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom"; //importo Link pra usar las rutas internas sin recargar la pagina
+import '../App.css';
 
 class Header extends Component {
 
     render() {
         return (
-            <header>
+            <header id="barra_nav">
                 <h1> Vaqueritos Peliculas </h1>
 
                 <nav>
-                    <ul>
+                    <ul id="menu">
                         <li><Link to="/" exact="true">Home</Link></li>
                         <li><Link to="/favoritos" >Favoritos</Link></li>
                         <li><Link to="/populares" >Peliculas Populares</Link></li>
